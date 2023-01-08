77a8d7f1d704988dd4a690b8303d1656b92da313
bBraian <braianvoficial@gmail.com>
2023-01-07 21:34:10 -0300

    letting companys info responsive

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -30,14 +30,32 @@ export const CompanyInfos = styled.div`
 `;
 
 export const CompanyLogo = styled.img`
-    height: 80px;
-    width: 80px;
+    height: 8.25rem;
+    width: 8.25rem;
     border-radius: 16px;
     position: absolute;
     bottom: 0;
+
+    @media (max-width: 1024px) {
+        height: 7.25rem;
+        width: 7.25rem;        
+    }
+
+    @media (max-width: 768px) {
+        height: 5rem;
+        width: 5rem;
+    } 
 `;
 
 export const CompanyTitle = styled.h1`
-    padding-left: 92px;
+    padding-left: 9.25rem;
     margin: 0.25rem 0;
+
+    @media (max-width: 1024px) {
+        padding-left: 8.25rem;     
+    }
+
+    @media (max-width: 768px) {
+        padding-left: 6rem;
+    } 
 `;
