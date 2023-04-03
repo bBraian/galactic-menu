0bde3b30e202db27c21479506b75b6568fe31b97
bBraian <braianvoficial@gmail.com>
2023-04-02 21:36:31 -0300

    feat: adding skeleton loader

--- src/pages/Client/index.jsx
+++ src/pages/Client/index.jsx
@@ -4,14 +4,18 @@ import { ClientBg, ClientBox, ClientList, ClientPhoto, ClientTitle, CompanyBgImg
 import bgimg from '../../assets/images/companybg.png';
 import company from '../../assets/images/companylogo.png';
 import { useNavigate } from "react-router-dom";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export function Client() {
     const [clients, setClients] = useState([]);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate()
 
     async function getClients() {
         const res = await api.get('clients/all');
         setClients(res.data);
+        setLoading(false);
     }
 
     function handleGoToClientMenu(clientURL) {
@@ -32,19 +36,44 @@ export function Client() {
                 </CompanyInfos>
             </Header>
             <ClientList>
-                {clients.map(c => (
-                    <ClientBox 
-                        key={c.id}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.9 }}
-                        onClick={() => handleGoToClientMenu(c.url)}
-                    >
-                        <ClientBg src={c.background} />
-                        <ClientPhoto src={c.logo} />
-                        <ClientTitle>{c.title}</ClientTitle>
-                    </ClientBox>
+                {loading ? 
+                <>
+                    <Skeleton 
+                        height={132} 
+                        baseColor="#c4c4c4" 
+                        highlightColor="#f1f1f1"
+                        borderRadius={16}
+                        count={3}
+                        style={{marginBottom: 24}}
+                    />
+                    <Skeleton 
+                        height={132} 
+                        baseColor="#c4c4c4" 
+                        highlightColor="#f1f1f1"
+                        borderRadius={16}
+                        count={3}
+                        style={{marginBottom: 24}}
+                    />
+                </>
+                : 
+                <>
+                    {clients.map(c => (
+                        <ClientBox 
+                            key={c.id}
+                            whileHover={{ scale: 1.05 }}
+                            whileTap={{ scale: 0.9 }}
+                            onClick={() => handleGoToClientMenu(c.url)}
+                        >
+                            <ClientBg src={c.background} />
+                            <ClientPhoto src={c.logo} />
+                            <ClientTitle>{c.title}</ClientTitle>
+                        </ClientBox>
 
-                ))}
+                    ))}
+                </>
+                    
+                }
+  
             </ClientList>
         </Container>
     )
